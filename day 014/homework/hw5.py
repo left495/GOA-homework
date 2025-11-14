8e4c65767968b2b7d73c8837a06d96f9da278e27
cotnepocxveria <pocxveriacotne@gmail.com>
2025-11-14 19:45:29 +0400

    commit

---
+++ day 014/homework/hw5.py
@@ -0,0 +1,10 @@
+# მომხმარებელს შემოატანინეთ რიცხვი სანამ თქვენს მიერ ჩაფიქრებულ რიცხვს არ შემოიტანს.
+
+i=int(input('enter any number'))
+
+while i!=21:
+    print('incorrect')
+    i=int(input('enter any number'))
+
+    
+print('correct')
