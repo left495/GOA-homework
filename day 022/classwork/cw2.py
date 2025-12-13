11fa4be2ae7ed103f16e23ddc89b1a9f9ec26fff
cotnepocxveria <pocxveriacotne@gmail.com>
2025-12-13 21:01:38 +0400

    commit

---
+++ day 022/classwork/cw2.py
@@ -0,0 +1,24 @@
+# 2)შექმენი შემდეგი სია -->  ["goga", 12 ,"irakli", False , "giorgi" , 200]
+
+# ---თქვენი დავალებაა რომ "goga" შეცვალეთ --> "vashli" ით
+
+# ---False  შეცვალეთ ---> True ით
+
+# --- 200 შეცვალეთ --> 159 ით
+
+# --- "irakli" შეცვალეთ ---> "saba" თი
+
+# ბოლოს დაპრინტეთ სია და ნახეთ შეიცვლა თუ არa
+
+
+names=["goga", 12 ,"irakli", False , "giorgi" , 200]
+
+names[0] = 'vashli'
+
+names[3] =  True
+
+names[5] = 159
+
+names[2] = 'saba'
+
+print(names)
