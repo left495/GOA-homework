3458212e60f9e298311a334faf69cb9700c903f7
cotnepocxveria <pocxveriacotne@gmail.com>
2025-12-14 14:38:20 +0400

    commit

---
+++ day 022/homework/hw3.py
@@ -0,0 +1,11 @@
+
+# 3)letters = ["x", "o", "z", "t", "g" , "a" ]
+
+
+# ინდექსების გამოყენებით ტერმინალში გამოიტანეთ(ააწყვეთ) "goga"
+
+
+letters =["x", "o", "z", "t", "g" , "a" ]
+
+
+print(letters[4],letters[1],letters[4],letters[5])
---
+++ day 022/homework/hw6.py
@@ -0,0 +1,11 @@
+
+# 6)შეცვალე სიის პირველი ელემენტი "Hello"-ით.
+# words = ["test", "python", "code"]
+
+words = ["test", "python", "code"]
+
+print(words)
+
+words[0]='hello'
+
+print(words)
