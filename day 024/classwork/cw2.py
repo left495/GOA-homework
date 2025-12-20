bc4e6fed9300fb0a2f723404c50fb280e9f49914
cotnepocxveria <pocxveriacotne@gmail.com>
2025-12-20 18:44:46 +0400

    commit

---
+++ day 024/classwork/cw2.py
@@ -0,0 +1,20 @@
+# # 
+# 2)შექმენით ია --> ["goga" , "irakli" ,'giorgi' , "saba" , "gela" ,"daviti"] ტერმინალში გამოიტანეთ irakli, saba და giorgi როგორც დადებით ასევე უარყოფითი ინდექსებით
+
+
+
+names= ["goga" , "irakli" ,'giorgi' , "saba" , "gela" ,"daviti"] 
+
+
+print(names[1])
+print(names[-5])
+
+print('========================')
+
+print(names[3])
+print(names[-3])
+
+print('========================')
+
+print(names[2])
+print(names[-4])
