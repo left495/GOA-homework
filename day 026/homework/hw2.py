bb291ba6f3fac0686007c27e7823caf8ea8f0954
cotnepocxveria <pocxveriacotne@gmail.com>
2025-12-26 22:28:23 +0400

    commit

---
+++ day 026/homework/hw2.py
@@ -0,0 +1,21 @@
+# 2) შექმენი ცარიელი სია. მომხმარებელს შეაყვანინე რიცხვები სანამ "stop"-ს არ დაბეჭდავს, ყოველი ახალი რიცხვი: თუ ნაკლებია 50-ზე → ჩასვი სიის დასაწყისში (insert), თუ მეტია ან ტოლია 50-ის → დაამატე ბოლოში (append), ბოლოს დაბეჭდე სია
+
+
+n=[]
+
+num=input('enter any numbers:')
+
+
+while True:
+    num=input('enter any numbers:')
+
+    if num=='stop':
+        print(n)
+        break
+
+    number=int(num)
+
+    if number<50:
+        n.insert(0,number)
+    else:
+        n.append(number)
