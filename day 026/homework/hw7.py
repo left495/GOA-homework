4397f8b3cc8c93be3fefd8f768012f7fdef4d79a
cotnepocxveria <pocxveriacotne@gmail.com>
2026-01-06 23:06:12 +0400

    commit

--- day 026/homework/hw7.py
+++ day 026/homework/hw7.py
@@ -1,3 +1,22 @@
 
 
 # 7) მომხმარებელს შემოაყვანინე რიცხვები, ეს რიცხვები დაამატე სიაში, თუ ორი მეზობელი ელემენტის ჯამი <50-ზე მაშინ წაშალე მეორე ელემენტი, დაბეჭდე საბოლოო სია.
+
+
+nums=[]
+
+count=int(input('how many munbers do u want ?'))
+
+for i in range(count):
+    nums.append(int(input('enter number:')))
+
+
+i=0
+
+while i<len(nums) -1:
+    if nums[i] + nums[i+1]<50:
+        nums.pop(i+1)
+    else:
+        i+=1
+
+print(nums)
