34d14015c1022e414806c293f2d5a49b3f86cbf5
cotnepocxveria <pocxveriacotne@gmail.com>
2026-02-14 14:59:41 +0400

    commit

---
+++ day 038/homework/hw1.py
@@ -0,0 +1,35 @@
+# codewars 1
+
+# def repeat_str(repeat, string):
+    # return repeat*string
+
+# codewars 2
+
+# def get_count(sentence):
+#     sum=0
+#     for i in sentence:
+#         if i in 'aeiou':
+#             sum+=1
+#     return sum
+            
+
+# codewars 3
+
+# def positive_sum(arr):
+#     sum=0
+#     for i in arr:
+#         if i>0:
+#             sum+=i
+#     return sum
+
+
+# codewars 4
+
+# def string_to_number(s):
+#     return int(s)
+
+
+# codewars 5
+
+# def greet():
+#     return 'hello world!'
