3db2f56a8e48f995e058eafb0e72c38e0f4d0e56
cotnepocxveria <pocxveriacotne@gmail.com>
2026-02-25 21:54:12 +0400

    commit

---
+++ day 042/homework/hw1.py
@@ -0,0 +1,48 @@
+
+# codewars 1.
+
+# def remove_char(s):
+#     finished=''
+
+#     for i in range(1,len(s)-1):
+#         finished+=s[i]
+#     return finished
+        
+# codewars 2.
+
+# def double_integer(i):
+#     return i*2
+
+# codewars 3.
+
+# def greet(name):
+    # return (f"Hello, {name} how are you doing today?")
+
+
+# codewars 4.
+
+# def basic_op(operator, value1, value2):
+#     if operator=='+':
+#         return value1 + value2
+#     elif operator=='-':
+#         return value1 - value2
+#     elif operator=='*':
+#         return value1 * value2
+#     else:
+#         return value1 / value2
+
+# codewars 5.
+
+# def sum_array(a):
+#     finished=0
+#     for i in a:
+#         finished+=i
+#     return finished
+
+# codewars 6.
+
+# def simple_multiplication(number) :
+#     if number%2==(0):
+#         return number * 8
+#     else:
+#         return number * 9
