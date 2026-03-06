9aea7dceb48b6c02e8e1e60439ba8534d6c71e93
cotnepocxveria <pocxveriacotne@gmail.com>
2026-03-06 22:32:37 +0400

    commit

---
+++ day 045/homework/hw1.py
@@ -0,0 +1,72 @@
+# codewars 1
+
+# def fun(num,value):
+#     dd= num*value
+#     da=dd.capitalize()
+#     return da
+
+
+
+# def accum(st):
+#     fin=''
+#     for i in range(len(st)): 
+#         if i==len(st)-1:
+#             fin+=fun(i+1,st[i])
+#         else:
+#             fin+=fun(i+1,st[i])
+#             fin+='-'
+#     return fin
+
+# codewars 2
+
+# def litres(time):
+#     return time//2
+
+
+# codewars 3
+
+# def to_jaden_case(string):
+#     listed_words=string.split()
+#     finished=''
+#     for i in range(len(listed_words)):
+#         if i==len(listed_words)-1:
+#             finished+=listed_words[i].capitalize()
+#         else:
+#             finished+=listed_words[i].capitalize()+' '
+#     return finished
+
+
+# codewars 4
+
+# def lovefunc( flower1, flower2 ):
+#     if flower2%2!=0 and flower1%2==0:
+#         return True
+#     elif flower2%2==0 and flower1%2!=0:
+#         return True
+#     else:
+#         return False
+
+
+# codewars 5
+
+# def maps(a):
+#     new=[]
+#     for i in a:
+#         new.append(i*2)
+#     return new
+        
+
+# codewars 6
+
+# def solution(text, ending):
+#     dd=len(ending)
+#     finished=''
+#     for i in range(len(text)-1,len(text)-dd-1,-1):
+#         finished+=text[i]
+
+#     da=finished[::-1]
+#     if da==ending:
+#         return True
+#     else:
+#         return False
+        
