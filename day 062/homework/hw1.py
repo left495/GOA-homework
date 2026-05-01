789335e0c422400110bc825f7640cde6d56fe9d7
cotnepocxveria <pocxveriacotne@gmail.com>
2026-05-01 17:48:29 +0400

    commit

---
+++ day 062/homework/hw1.py
@@ -0,0 +1,26 @@
+
+# codewars 1
+
+# def reverse_number(n):
+#     ned=str(n)
+#     last=''
+#     if ned[0]=='-':
+#         last+='-'
+#         dd=ned[::-1]
+#         last+=dd
+#         return int(last[0:len(last)-1])
+#     else:
+#         return int(ned[::-1])
+
+
+# codewars 2
+
+
+# def create_phone_number(n):
+#     new=[]
+#     for i in n:
+#         new.append(str(i))
+        
+#     return f"({new[0]}{new[1]}{new[2]}) {new[3]}{new[4]}{new[5]}-{new[6]}{new[7]}{new[8]}{new[9]}"
+        
+    
