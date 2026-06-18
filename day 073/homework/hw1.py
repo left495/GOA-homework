2632ccb12f66039165b529f2915979076bfeee44
left495 <left9812@gmail.com>
2026-06-18 20:26:06 +0400

    commit

--- day 073/homework/hw1.py
+++ day 073/homework/hw1.py
@@ -1,30 +1,49 @@
-def mxdiflg(a1, a2):
-    if len(a1)==0 or len(a2)==0:
-        return -1
-    a1max=0
-    a1min=50
-    for i in a1:
-        if a1max<len(i):
-            a1max=len(i)
-        elif a1min>len(i):
-            a1min=len(i)
-    a2max=0
-    a2min=50
-    
-    for j in a2:
-        if a2max<len(j):
-            a2max=len(j)
-        elif a2min>len(j):
-            a2min=len(j)
-            return len(j)
-    fir=a1max-a2min
-    sec=abs(a1min-a2max)
-    
+# codewars 1
 
+# def mxdiflg(a1, a2):
+#     if len(a1)==0 or len(a2)==0:
+#         return -1
+#     a1max=0
+#     a1min=50
+#     for i in a1:
+#         if a1max<len(i):
+#             a1max=len(i)
+#         if a1min>len(i):
+#             a1min=len(i)
+#     a2max=0
+#     a2min=50
     
+#     for j in a2:
+#         if a2max<len(j):
+#             a2max=len(j)
+#         if a2min>len(j):
+#             a2min=len(j)
+#     fir=abs(a1max-a2min)
+#     sec=abs(a1min-a2max)
+#     if fir>sec:
+#         return fir
+#     else:
+#         return sec
     
-s1 = ["ejjjjmmtthh", "zxxuueeg", "aanlljrrrxx", "dqqqaaabbb", "oocccffuucccjjjkkkjyyyeehh"]
-s2 = ["bbbaaayddqbbrrrv"]
+# codewars 2
 
 
-print(mxdiflg(s1,s2))
+
+
+
+# codewars 3
+
+
+
+# codewars 4
+
+
+# def find_longest(arr):
+#     da=0
+#     dd=0
+#     for i in arr:
+#         i=str(i)
+#         if len(i)>dd:
+#             dd=len(i)
+#             da=int(i)
+#     return da
