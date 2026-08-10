ec342d1f77418b2b067333420c2f24c0003eef01
left495 <left9812@gmail.com>
2026-08-10 17:10:45 +0400

    commit

---
+++ day 089/homework/hw1.js
@@ -0,0 +1,201 @@
+
+
+// let sen = "I like cats"
+
+
+// console.log(sen.replace('cats','dogs'))
+
+// // 1)შექმენი სტრინგი:
+
+// let text = "I like cats";
+
+// replace() მეთოდის გამოყენებით შეცვალე "cats" სიტყვა "dogs"-ით.
+
+
+// let text = "Hello Goga"
+
+// text=text.replace('Goga','Nika')
+
+// console.log(text)
+
+// 2)let text = "Hello Goga";
+
+// შეცვალე "Goga" სიტყვა "Nika"-ით და დაბეჭდე შედეგი.
+
+
+// let text = "apple banana apple orange apple"
+
+// text=text.replaceAll('apple','mango')
+
+// console.log(text)
+
+
+// 3)let text = "apple banana apple orange apple";
+
+// replaceAll()-ის გამოყენებით ყველა "apple" შეცვალე "mango"-თი.
+
+// let text = "JavaScript is easy. JavaScript is fun."
+
+
+// console.log(text.replaceAll('JavaScript','Python'))
+
+// 4)let text = "JavaScript is easy. JavaScript is fun.";
+
+// ყველა "JavaScript" შეცვალე "Python"-ით.
+
+// let text = "JavaScript"
+
+// console.log(text.slice(0,4))
+
+
+// 5)let text = "JavaScript";
+
+// slice()-ის გამოყენებით გამოიტანე:
+
+// Java
+
+// let text = "Programming"
+
+// console.log(text.slice(3,7))
+
+
+// 6)let text = "Programming";
+
+// slice()-ის გამოყენებით გამოიტანე:
+
+// gram
+
+// let score = 50
+
+// score=100
+
+// console.log(score)
+
+
+// 7)შექმენი:
+
+// let score = 50;
+
+// შემდეგ შეცვალე score-ის მნიშვნელობა 100-ით და დაბეჭდე.
+
+// let number = 10
+
+// number+=5
+
+// console.log(number)
+
+// 8)let number = 10;
+
+// += ოპერატორის გამოყენებით დაუმატე 5.
+
+
+// let money = 100
+
+// money-=30
+
+// console.log(money)
+
+
+// 9)let money = 100;
+
+// -= ოპერატორის გამოყენებით გამოაკელი 30.
+
+// დაბეჭდე რესულტი
+
+// let number = 5
+
+// number*=4
+
+// console.log(number)
+
+
+
+// 10)
+// let number = 5;
+// *= ოპერატორის გამოყენებით გაამრავლე 4-ზე.
+
+// let number = 100
+
+// number/=5
+
+// console.log(number)
+
+
+// 11)let number = 100;
+
+// /= ოპერატორის გამოყენებით გაყავი 5-ზე.
+
+// let balance = 500
+
+// balance-=100
+
+// balance+=200
+
+// balance-=50
+
+// balance*=2
+
+// console.log(balance)
+// 12)let balance = 500;
+
+// წარმოიდგინე, რომ:
+
+// დახარჯე 100 → -=
+// დაგერიცხა 200 → +=
+// დახარჯე 50 → -=
+// თანხა გაორმაგდა → *=
+
+// ბოლოს გამოიტანე balance.
+
+// let num =10
+
+// num++
+
+// console.log(num);
+
+
+// num--
+
+// console.log(num);
+
+
+// 13)შექმენი ცვლადი და შეინმახე რიცხვი
+
+// გაზარდე ეს რიცხვი 1 ით და დაბეჭდე , შემდეგ შშეამცირე 1 ით და დაბეჭდე
+
+// let age = 15
+// const country = "Georgia"
+
+// age=10
+
+// country='armenia'
+
+
+// 14)შექმენი ორი ცვლადი:
+
+// let age = 15;
+// const country = "Georgia";
+
+// age შეცვალე სხვა მნიშვნელობით.
+
+// შემდეგ სცადე country-ს შეცვლა.
+
+// დააკვირდი განსხვავებას.
+
+let price = 100
+let quantity = 3
+
+console.log(price*quantity)
+
+price-=20
+
+console.log(price*quantity)
+
+
+
+// 15)let price = 100;
+// let quantity = 3;
+
+// გამოთვალე ჯამური ფასი price და quantity-ის გამოყენებით.
+
+// შემდეგ price-ს 20 დააკელი -= ოპერატორით და თავიდან გამოთვალე ჯამური ფასი.
