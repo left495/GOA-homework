2065a9e91309efcdd29d2b8f266c93cb059dd03f
left495 <left9812@gmail.com>
2026-08-13 13:19:01 +0400

    commit

---
+++ day 091/classwork/cw1.js
@@ -0,0 +1,35 @@
+
+
+
+// 1)შექმენით ცვლადი და ეინახეთ რაიმე რიცხვი , შენი დავალებაა
+//  შეამოწმო თ ეს ცვლადში შე ნახული რიცხვი არის დადებითი დაბეჭდე 
+// კონსოლში"positive"  , თუ ეს რიცხვი არის უარყოფით დაბეჭდე "negative"
+//  სხვა შემთხვევაში დაბეჭდე "zero"
+
+
+
+// 2)შექმენი ცვლადი სადაც შეინახავ სახელს  , შემდეგ შეადარე თუ ცვლადში 
+// შენახული სახელი უდრის თქვენს სახელს გამოიტანე კონსოლში --> "we have same name" 
+// სხვა შემთხვევაში გამოუტანე --> "we do not have same name"
+
+let num = 38
+
+if (num>0){
+    console.log("positive")
+}else if (num<0){
+    console.log('negative')
+}else{
+    console.log('zero')
+}
+
+
+
+let name = 'tsotne'
+
+
+if (name=='tsotne'){
+    console.log("we have same name")
+}else{
+    console.log("we do not have same name")
+    
+}
